391f0153d29818fb5569103246b86444d9b4f5ea
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-05-26 15:35:54 -0400

    add header and empty components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
 import "components/Appointment/styles.scss";
+
+import Header from "./Header";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
 
@@ -8,3 +14,13 @@ export default function Appointment(props) {
     <article className="appointment"></article>
    )
 }
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+
