92dce879ca2e01ae110b76221518756d211d3b8a
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-06-02 12:22:05 -0400

    create getappointmentforday function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,22 @@
+export function getAppointmentsForDay(state, day) {
+  let appointments = [];
+
+  // loop through state days to find the day
+  for (let i=0; i < state.days.length; i++) {
+
+    if (state.days[i].name === day) {
+      let appointmentsIds = state.days[i].appointments;
+
+      // loop through appointments array to select all appointment objects from state.appointments
+      for (let j=0; j < appointmentsIds.length; j++) {
+
+          let appointmentId = appointmentsIds[j];
+
+          appointments.push(state.appointments[appointmentId]);
+
+      }
+    }
+  }
+
+  return appointments;
+}
